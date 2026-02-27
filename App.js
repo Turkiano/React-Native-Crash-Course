03761979c0a1330955a2477ab796c38671f5c3db
Turkiano <turki.saeed.ksa@gmail.com>
2026-02-28 01:19:58 +0300

    testing the tab bar with two Navigation

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+// Home Screen
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>🏠 Home Screen</Text>
+    </View>
+  );
+}
+
+// Favorites Screen
+function FavoritesScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>❤️ Favorites Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>يا أهلا وسهلا تركيانو</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Favorites" component={FavoritesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
